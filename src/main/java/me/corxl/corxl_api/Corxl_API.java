4228997dc4155812a52655219e828d1837c89528
BuildTools <unconfigured@null.spigotmc.org>
2021-12-09 18:06:00 -0800

    Test commit

--- src/main/java/me/corxl/corxl_api/Corxl_API.java
+++ src/main/java/me/corxl/corxl_api/Corxl_API.java
@@ -25,6 +25,7 @@ public final class Corxl_API extends JavaPlugin {
         Bukkit.addRecipe(spongeRecipe());
         Bukkit.addRecipe(prismarineShardRecipe());
         System.out.println("Change commit1");
+        System.out.println("Change 2");
         this.getServer().getPluginManager().registerEvents(new EndPoralListener(), this);
         this.getServer().getPluginManager().registerEvents(new ItemEnchantListener(), this);
         this.getCommand("cxl").setExecutor(new Commands());
